22b568e5a95acf27dbe14501c1b984072addedac
Hafid Selbi <hselbi@e1r12p3.1337.ma>
2023-11-06 22:20:30 +0100

    new updates 2

--- front-end/src/components/ChatElements.tsx
+++ front-end/src/components/ChatElements.tsx
@@ -33,7 +33,8 @@ const ChatElements = (id: IdType) => {
   const { contact, profile } = useAppSelector(state => state);
   const dispatch = useAppDispatch();
   const selected_id = id.room_id;
-  console.log(id);
+  console.log('--->', id);
+  console.log('--->', id);
   const selectedChatId = contact.room_id;
   let isSelected = +selectedChatId === id.id;
 
